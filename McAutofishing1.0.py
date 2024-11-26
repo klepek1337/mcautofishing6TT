584a067112512c86368817d44828ded4f89fb380
klepek1337 <134932596+klepek1337@users.noreply.github.com>
2024-11-26 11:54:28 +0100

    Updated automatic image detection from photos in C

--- McAutofishing1.0.py
+++ McAutofishing1.0.py
@@ -1,42 +1,58 @@
 import pyautogui
 import time
 import threading
-print("Skrypt na wędkę by klepek, Ultimate build 1.0")
+import os
+
+print("Script Minecraft pour Piocher, fait par Adrian")
 print("GUI scale 3, Show subtitles")
+
+image_directory = os.path.join(os.path.expanduser("~"), "Images")
+gigawedka_path = os.path.join(image_directory, "gigawedka.png")
+playerhurts_path = os.path.join(image_directory, "playerhurts.png")
+
 def gigawedkaeng():
     try:
-        return pyautogui.locateOnScreen('gigawedka.png', confidence=0.8, grayscale=True) is not None
+        return pyautogui.locateOnScreen(gigawedka_path, confidence=0.8, grayscale=True) is not None
     except pyautogui.ImageNotFoundException:
         return False
-    
+
 def megagigasusattackfromnigger():
     try:
-        return pyautogui.locateOnScreen('playerhurts.png', confidence=0.85, grayscale=True) is not None
+        return pyautogui.locateOnScreen(playerhurts_path, confidence=0.85, grayscale=True) is not None
     except pyautogui.ImageNotFoundException:
         return False
-    
+
 def main():
     print("Rightclick")
     wejscie()
     gigawedkadetector()
     main()
+
 def wejscie():
     print("wejscie")
     time.sleep(5)
     pyautogui.rightClick()
+
 def gigawedkadetector():
-    while gigawedkaeng() is False:
+    while not gigawedkaeng():
         time.sleep(0.1)
     print("złapano")
     pyautogui.rightClick()
 
 def megagigasusattackfromniggerdetector():
-    while megagigasusattackfromnigger() is False:
+    while not megagigasusattackfromnigger():
         time.sleep(0.1)
-    print("Critical demage, ESC")
+    print("Critical damage, ESC")
     pyautogui.keyDown('esc')
+    pyautogui.keyUp('esc')
 
 if __name__ == "__main__":
+
+    if not os.path.exists(image_directory):
+        print(f"Directory does not exist: {image_directory}")
+    else:
+        print(f"Using images from: {image_directory}")
+
     mainstart = threading.Thread(target=main)
     susstart = threading.Thread(target=megagigasusattackfromniggerdetector)
     mainstart.start()
